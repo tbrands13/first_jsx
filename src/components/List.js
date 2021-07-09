26bc597cbd99754d7155f96c51f26837461d5ed9
Titus Brandy <tbrands13@gmail.com>
2021-07-08 22:13:10 -0500

    INIT COMMIT

---
+++ src/components/List.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class List extends Component{
+    render(){
+        return(
+            <div>
+                <ul>
+                    <li>Learn React</li>
+                    <li>Climb Mt. Everest</li>
+                    <li>Run a marathon</li>
+                    <li>Feed the dogs</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default List;
